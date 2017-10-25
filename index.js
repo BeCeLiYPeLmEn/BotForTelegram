6c584e37b6ee43be7587026f14e6c6ec1ebc7d4a
BeCeLiYPeLmEn <nikitafil.1234@mail.ru>
2017-10-26 00:04:11 +0400

    added db to bot

--- index.js
+++ index.js
@@ -8,7 +8,16 @@ const bot = new TelegramBot(token, { polling: true });
 
 const tasksInfo = require ('./tasksInfo.js');
 
-const users = {};
+const low = require('lowdb')
+const FileSync =  require ('lowdb/adapters/FileSync')
+
+const adapter = new FileSync ('db.json')
+const db = low(adapter)
+
+db.defaults ({ users: []}).write();
+
+
+
 // <----Buttons---->
 const startMenu = JSON.stringify({
      inline_keyboard: [
@@ -25,52 +34,60 @@ const leave = JSON.stringify({
 
 // <----The functional responsible for verifying the answers---->
 
-bot.onText(/\/start/, (msg,match) => {
-    users [msg.chat.id]={task:0, correct:0, wrong:0, leaveMsgId:0};
-    bot.sendMessage(msg.chat.id,"Чего ты хочешь?", {reply_markup: startMenu});
+bot.onText(/.+/,(msg,match) =>{
+    if(msg.text == '/start'){
+        if(!db.get('users').find({id:msg.chat.id}).value())
+            db.get('users').push({id: msg.chat.id, task:0, correct:0, wrong:0, leaveMsgId:0}).write();
+        bot.sendMessage(msg.chat.id,"Чего ты хочешь?", {reply_markup: startMenu});
+    }
+    else {
+        let user = db.get('users').find ({id:msg.chat.id}).value();
+        if (user.task!=0) {
+            if (user.task.answer==msg.text){
+            bot.editMessageReplyMarkup( null, {chat_id: msg.chat.id, message_id: user.leaveMsgId});
+            user.leaveMsgId=0;
+            bot.sendMessage(msg.chat.id,"Все правильно! Хочешь еще?",{reply_markup:startMenu});
+            user.correct++;
+            user.task=0;
+        }
+        else bot.sendMessage(msg.chat.id, "Не правильно");
+        db.get('users').find({id:msg.chat.id}).assign(user).write();
+    }
+  
+  }
+
 });
     
-        bot.onText(/.+/,(msg,match) => {
-            if (users[msg.chat.id].task.getTaskInfo = true) {
-            if (users[msg.chat.id].task!=0) {
-                if (users[msg.chat.id].task.answer==msg.text){ 
-                    bot.editMessageReplyMarkup( null, {chat_id: msg.chat.id, message_id:users[msg.chat.id].leaveMsgId});
-                    users[msg.chat.id].leaveMsgId=0;
-                    bot.sendMessage(msg.chat.id,'Все правильно! Хочешь еще?', {reply_markup: startMenu});
-                    users[msg.chat.id].correct++;
-                    users[msg.chat.id].task=0;
-                }
-                else bot.sendMessage(msg.chat.id, "Не правильно");
-            }
-        }
-        });
+ 
     
 //<----The functional responsible for issuing tasks and statistics ---->
    bot.on('callback_query',cbQuery => {
     const chatId = cbQuery.message.chat.id;
     const msgId = cbQuery.message.message_id;
+    let user = db.get('users').find({id: chatId}).value();
      if (cbQuery.data=="getTask"){
-        users [chatId].task= tasksInfo[Math.floor(Math.random() * tasksInfo.length)]; 
+        if(user.task==0) user.task = tasksInfo[Math.floor(Math.random() * tasksInfo.length)]; 
         bot.editMessageText('Загружаем задание', {chat_id: chatId, message_id:msgId});
-        bot.sendPhoto(chatId,__dirname+'/./images/'+users[chatId].task.img,{reply_markup: leave})
+        bot.sendPhoto(chatId,__dirname+'/./images/'+user.task.img,{reply_markup: leave})
            .then(data => {
-            users [chatId].leaveMsgId = data.message_id;
+            user.leaveMsgId = data.message_id;
             bot.editMessageText('Напиши мне ответ',{chat_id : chatId, message_id:msgId})});
      }
      if (cbQuery.data=='leave'){
-        users[chatId].wrong++;
+        user.wrong++;
         bot.editMessageReplyMarkup(null, {chat_id: chatId, message_id: msgId});
-        bot.sendMessage(chatId,'Правильный ответ: '+ users [chatId].task.answer);
+        bot.sendMessage(chatId,'Правильный ответ: '+ user.task.answer);
         bot.sendMessage(chatId, 'Может еще?', {reply_markup:startMenu})
-        users[chatId].task=0;
+        user.task=0;
      }  
      if (cbQuery.data=='stat'){
-        const cor = users[chatId].correct;
-        const wr = users[chatId].wrong;
+        const cor = user.correct;
+        const wr = user.wrong;
         bot.editMessageText(
             'Правильных: '+cor+'\nНерешённых: '+wr+'\nПроцент решённых: '+cor/(cor+wr)*100+'%',
             {reply_markup:startMenu, chat_id: chatId, message_id: msgId});
      }
+     db.get('users').find({id: msgId}).assign(user).write();
 
  });    
    console.log('Сервер запущен')
